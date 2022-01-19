b3160d3e764145f18a75ce3c6053efb76cd48970
Joey <jw453119@ohio.edu>
2022-01-19 17:26:29 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joey Whitehead
+ *        Date: 1-19-2022
+ * Description: Basic Hello World test program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
